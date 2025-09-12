221af5f85986432002a80212c6b0e74a9db6a530
RobCMYK <robemartin04@hotmail.com>
2025-09-12 18:49:23 -0500

    initializing

---
+++ app.mjs
@@ -0,0 +1,42 @@
+import 'dotenv/config'; // Load environment variables from .env file
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+//const mongoUri = process.env.MONGO_URI;         Make .env
+//console.log("is env working? ", mongoUri);
+
+const client = new MongoClient(process.env.MONGO_URI, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("admin").command({ ping: 1 });
+        console.log("Connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.use(express.urlencoded  ({ extended: true })); // Parse form data
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the "public" directory
+app.use(express.json()); // Parse JSON bodies
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'miniapp.html'));
+});
---
+++ knowledge.md
@@ -0,0 +1,1 @@
+# Knowledge Article: Agile Handoffs
